c0dae885c97c6d0dec9ef4eaf63cebcce59ca114
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-30 23:27:25 +0530

    update navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,25 @@
 import React from "react";
+import styles from "../../styles/components/header/Header.module.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <div className="logo">
-        <img src="" alt="" />
+        <img src="#" alt="logo" />
       </div>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Products</li>
-          <li>Cart</li>
-          {/* condition will apply bw login and logout */}
-          <li>Login</li>
-          {/* <li>Logout</li> */}
-        </ul>
+      <nav className={styles.navlinks}>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/about"}>About</Link>
+        <Link href={"/products"}>Products</Link>
+        <Link href={"/products/:id"}>Cart</Link>
+        {/* condition will apply bw login and logout */}
+        <Link href={"/login"}>Login</Link>
+        {/* <Link href={"/logout"}>Logout</Link> */}
       </nav>
-      <button>Profile</button>
+      <Link href={"/profile"} className={styles.btn}>
+        <button>Profile</button>
+      </Link>
     </header>
   );
 }
