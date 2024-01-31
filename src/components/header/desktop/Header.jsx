597ff660a3cff0190309320a9c60fc26851c918d
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-31 22:55:01 +0530

    navbar update

--- src/components/header/desktop/Header.jsx
+++ src/components/header/desktop/Header.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import styles from "../../styles/components/header/Header.module.css";
+import styles from "./../../../styles/components/header/desktop/header.module.css";
 import { Link } from "react-router-dom";
+import { CiUser } from "react-icons/ci";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className="logo">
-        <img src="#" alt="logo" />
+      {/* logo */}
+      <div className={styles.logo}>
+        {/* <img src="#" alt="logo" /> */}
+        Buys-Mart
       </div>
+
+      {/* navlinks */}
       <nav className={styles.navlinks}>
         <Link href={"/"}>Home</Link>
         <Link href={"/about"}>About</Link>
@@ -17,9 +23,20 @@ export default function Header() {
         <Link href={"/login"}>Login</Link>
         {/* <Link href={"/logout"}>Logout</Link> */}
       </nav>
+
+      {/* userProfile */}
       <Link href={"/profile"} className={styles.btn}>
-        <button>Profile</button>
+        <div className={styles.userProfile}>
+          <CiUser />
+        </div>
       </Link>
+
+      {/* slidebar opener*/}
+      <div className={styles.slidebar}>
+        <FaBarsStaggered />
+      </div>
+
+      <slidebar />
     </header>
   );
 }
